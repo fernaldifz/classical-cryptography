467e6abd5d0cd6bf59c490118507cc7d0719c9da
fernaldifz <fernaldifz@gmail.com>
2022-02-01 17:04:52 +0700

    add enigma cipher, requirements, venv

--- src/enigmaCipher.py
+++ src/enigmaCipher.py
@@ -1,8 +1,25 @@
-def generateEnigmaRotor():
-    pass
+from enigma.machine import EnigmaMachine
 
-def encryptTextEnigma():
-    pass
+def initEnigma(wheelOrder, ringSettings, reflector, plugboardSettings):
+    machine = EnigmaMachine.from_key_sheet(
+        rotors = wheelOrder,
+        ring_settings = ringSettings,
+        reflector = reflector,
+        plugboard_settings = plugboardSettings
+    )
+    return machine
 
-def decryptTextEnigma():
-    pass
+def enigma(machine, initPosition, messageKey, encryptedText):
+    machine.set_display(initPosition)
+    messageKey = machine.process_text(messageKey)
+    machine.set_display(messageKey)
+
+    text = machine.process_text(encryptedText, replace_char='Z')
+
+    return text
+
+def rotorPosition(machine):
+    return machine.get_display()
+
+# machine = initEnigma('II IV V', [2, 18, 7], 'B', 'AV BS CG DL FU HZ IN KM OW RX')
+# print(enigma(machine, 'WXC', 'KCH', 'RKBNUTBJUIFAIYKLERJ'))
