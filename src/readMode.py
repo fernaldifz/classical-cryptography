cc58bcbe2d72213c90580dc1939a015ec5dc2d36
wpo9nine <wpo9nine@gmail.com>
2022-02-04 05:54:31 +0700

    add read mode

---
+++ src/readMode.py
@@ -0,0 +1,23 @@
+def noSpace(string):
+    newString = ""
+    upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for char in string:
+        if char in upperCase:
+            newString += char
+    
+    return newString
+
+def spaceFive(string):
+    newString = ""
+    n = 5
+    
+    split_strings = [string[index : index + n] for index in range(0, len(string), n)]
+    
+    for string in split_strings:
+        newString += string + " "
+
+    return newString
+
+# string = noSpace("AKU CINTA PADAMU")
+# print(string)
+# print(spaceFive(string))
